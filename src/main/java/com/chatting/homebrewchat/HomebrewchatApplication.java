73385b81ac0b0f37fd9b549652b4be03bdf69354
sok5188 <sok5188@gmail.com>
2023-04-28 20:28:32 +0900

    init

---
+++ src/main/java/com/chatting/homebrewchat/HomebrewchatApplication.java
@@ -0,0 +1,13 @@
+package com.chatting.homebrewchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebrewchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebrewchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatting/homebrewchat/HomebrewchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatting.homebrewchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebrewchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
