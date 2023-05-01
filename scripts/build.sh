43c4c17885e8006073c62ddda0bd2244f7c2399d
WONGYUN SHIN <43509682+sok5188@users.noreply.github.com>
2023-05-01 20:44:46 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PROJECT_NAME="github_action"
-JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
+PROJECT_NAME="be"
+JAR_PATH="/home/ubuntu/$PROJECT_NAME/build/libs/*.jar"
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
-DEPLOY_LOG_PATH="/home/ubuntu/github_action/deploy.log"
-DEPLOY_ERR_LOG_PATH="/home/ubuntu/github_action/deploy_err.log"
-APPLICATION_LOG_PATH="/home/ubuntu/github_action/application.log"
+DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
+APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
 
