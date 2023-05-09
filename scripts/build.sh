4f0f346ec5a6740902648a4fbbf2e2dcb9865033
WONGYUN SHIN <43509682+sok5188@users.noreply.github.com>
2023-05-09 14:18:23 +0900

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,20 @@ JAR_NAME=$(basename $BUILD_JAR)
 
 echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 
+
+echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 동작중인 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
+else
+  echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
+  echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
+  echo "> kill -9 $CURRENT_PID" >> $DEPLOY_LOG_PATH
+  kill -9 $CURRENT_PID
+fi
+
 echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
 echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 cp $BUILD_JAR $DEPLOY_PATH
@@ -17,18 +31,7 @@ cp $BUILD_JAR $DEPLOY_PATH
 sudo service nginx restart
 
 
-# echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
-# CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-# if [ -z $CURRENT_PID ]
-# then
-#   echo "> 현재 동작중인 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
-# else
-#   echo "> 현재 동작중인 어플리케이션 존재 O" >> $DEPLOY_LOG_PATH
-#   echo "> 현재 동작중인 어플리케이션 강제 종료 진행" >> $DEPLOY_LOG_PATH
-#   echo "> kill -9 $CURRENT_PID" >> $DEPLOY_LOG_PATH
-#   kill -9 $CURRENT_PID
-# fi
 
 # DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 # echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
